1295f9b9acfe4cd41cc8ef1639ff5611f8c4d52d
AyalaSprei <ayala0bsd@gmail.com>
2024-07-01 16:56:47 +0300

    commit

--- ToDoList/TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
+++ ToDoList/TodoApi/obj/Debug/net7.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f32b3b901a85ccb33824c3477865806cfbc1ca99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147d88b06072e4513b0d684d91318363365665b7")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
